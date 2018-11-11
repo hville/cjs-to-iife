d9a66ced0b42bc8de2bb59ddbc232be7b0a04966
hville <hugov@runbox.com>
2018-11-11 07:04:31 -0500

    updates

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 const ct = require('cotest'),
 			bundle = require('./index')
 
-ct.timeout(10000)
-
-ct('bundled script', async end => {
+ct('bundled script', async (t,end) => {
 	const code = await bundle('./node_modules/cotest/index.js')
 	eval(code)
-	ct('{==}', Object.keys(this.exports), ['default'])
-	ct('===', typeof this.exports.default, 'function')
-	ct('===', this.exports.default('fake'), undefined)
+	t('{==}', Object.keys(this.exports), ['default'])
+	t('===', typeof this.exports.default, 'function')
+	t('===', this.exports.default('fake'), undefined)
 	end()
 })
